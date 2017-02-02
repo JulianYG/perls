49af9f4e8a8ee4a77663a9676a1a687431f0b2bf
Julian <yg1811@hotmail.com>
2017-02-02 11:57:51 -0800

    finished two arms src and script code

--- pybullet/engine.py
+++ pybullet/engine.py
@@ -1,13 +1,13 @@
 import pybullet as p
 from rl import BulletPhysicsVR
-from render import generate_trajectory
+# from render import generate_trajectory
 
 
 
 # generate_trajectory((6,6,6), (0,0,0), 'see.csv', 'newsee.csv')
-simulator = BulletPhysicsVR(p, 2)
-1.4,-0.2,0.6
+simulator = BulletPhysicsVR(p, [2, 4], task=1, hand=False)
+
 simulator.set_camera_view(.8, -.2, 1, 0, -90, 120, 1)
-# simulator.replay('see.csv', 1)
-simulator.replay('see.csv')
-# simulator.replay('newsee.csv')
+
+simulator.replay('two_arms.csv', saveVideo=0)
+# simulator.record('two_arms_hanoi.csv')
--- pybullet/rl.py
+++ pybullet/rl.py
@@ -7,16 +7,19 @@ import numpy as np
 
 class BulletPhysicsVR(object):
 
-	def __init__(self, pybullet, kuka_id, task=0):
+	def __init__(self, pybullet, gripper_id, task=0, hand=False):
+		# Create tasks as first step
 		self.tasks = self._init_task()
 		self.task = task
 		self.p = pybullet
 
 		self.CONTROLLER_ID = 0
-		self.KUKA_GRIPPER_ID = kuka_id
+		self.GRIPPER_ID = gripper_id
+		self.VR_HAND_ID = None
 		self.POSITION = 1
 		self.BUTTONS = 6
 
+		# Default settings for camera
 		self.FOCAL_POINT = (0., 0., 0.)
 		self.YAW = 35.
 		self.PITCH = 50.
@@ -24,17 +27,25 @@ class BulletPhysicsVR(object):
 		self.FOCAL_LENGTH = 5.
 		self.UP_AX_IDX = 2
 
-		self.OBJ_CNT = self.KUKA_GRIPPER_ID + 1
+		self.ROBOT_MAP = {}
+		self.OBJ_CNT = max(gripper_id) + 1
 		self.tracking_obj = None
 		self.viewMatrix = None
 		self.projectionMatrix = None
+		self.hand = hand
 
 	def _load_task(self, flag):
+		"""
+		Load task for both recording and replay
+		"""
 		try:
 			if flag:
 				self.p.connect(self.p.GUI)
 				self.p.setInternalSimFlags(0)
 				self.p.setRealTimeSimulation(0)
+				# Use GUI and turn off simulation for replay
+				# In order to generate deterministic paths
+				# convenient for video recording
 			else:
 				self.p.connect(self.p.SHARED_MEMORY)
 				self.p.setInternalSimFlags(1)
@@ -64,12 +75,14 @@ class BulletPhysicsVR(object):
 				events = self.p.getVREvents()
 
 				for e in (events):
-					# If the user think one task is completed
+					# If the user think one task is completed, 
+					# he/she will push the menu button
 					if (e[self.BUTTONS][1] & self.p.VR_BUTTON_WAS_TRIGGERED):
 							# self.p.resetSimulation()
 							# self.p.removeAllUserDebugItems()
 						self.p.addUserDebugText('good job!', (1.7, 0, 1), (255, 0, 0), 12, 10)
-						# Can add line for mark here		
+						# Can add line for mark here
+						# so that in saved csv file, we know when one task is complete		
 
 				# Saving events routine
 				if events and events[0][5] > 0:	# Only take record when moving events happen
@@ -78,12 +91,18 @@ class BulletPhysicsVR(object):
 					time_elapse = curr_time - prev_time
 					prev_time = curr_time
 					for o_id in self.tracking_obj:
+						# Track objects
 						row = [(o_id)] + list(self.p.getBasePositionAndOrientation(o_id)[0]) + list(self.p.getBasePositionAndOrientation(o_id)[1])
 						writer.writerow(row)
-					gripper_info = [self.KUKA_GRIPPER_ID] + list(self.p.getBasePositionAndOrientation(self.KUKA_GRIPPER_ID)[0]) + \
-						list(self.p.getBasePositionAndOrientation(self.KUKA_GRIPPER_ID)[1])
+					# Write extra vr_hand now
+					if self.hand:
+						hand = [self.VR_HAND_ID] + list(self.p.getBasePositionAndOrientation(self.VR_HAND_ID)[0]) + list(self.p.getBasePositionAndOrientation(self.VR_HAND_ID)[1])
+						writer.writerow(hand)
+					for gripper_id in self.GRIPPER_ID:
+						gripper_info = [gripper_id] + list(self.p.getBasePositionAndOrientation(gripper_id)[0]) + \
+							list(self.p.getBasePositionAndOrientation(gripper_id)[1])
+						writer.writerow(gripper_info)
 					delay = [-1, time_elapse]
-					writer.writerow(gripper_info)
 					writer.writerow(delay)
 
 		except KeyboardInterrupt:
@@ -93,11 +112,11 @@ class BulletPhysicsVR(object):
 		load_status = 0
 		while load_status == 0:
 			load_status = self._load_task(1)
-
+		# Setup the camera 
 		self.p.setCameraViewPoint(self.FOCAL_POINT[0], self.FOCAL_POINT[1], self.FOCAL_POINT[2], 
 			self.PITCH, self.YAW, self.FOCAL_LENGTH)
 
-		r, g = self._setup_robot()
+		self._setup_robot()
 		f = open(file, 'r')
 		reader = csv.reader(f)
 		delay = 0
@@ -106,26 +125,27 @@ class BulletPhysicsVR(object):
 				delay = float(row[1]) / 12
 			else:
 				time.sleep(delay)
-				if int(row[0]) != self.KUKA_GRIPPER_ID:
-					self.p.resetBasePositionAndOrientation(int(row[0]) - self.KUKA_GRIPPER_ID - 1, (float(row[1]), float(row[2]), 
+				# Keep the simulation synced
+				if int(row[0]) not in self.GRIPPER_ID:
+					self.p.resetBasePositionAndOrientation(int(row[0]) - max(self.GRIPPER_ID) - 1, (float(row[1]), float(row[2]), 
 						float(row[3])), (float(row[4]), float(row[5]), float(row[6]), float(row[7])))
 				else:
+					# Assert load order for the plane, robot, and gripper are the same
 					eef_pos = (float(row[1]), float(row[2]), float(row[3]))
 					eef_orien = (float(row[4]), float(row[5]), float(row[6]), float(row[7]))
-					joint_pos = self.p.calculateInverseKinematics(r, 6, eef_pos, eef_orien)
-					self.p.resetBasePositionAndOrientation(g[0], eef_pos, eef_orien)
+					joint_pos = self.p.calculateInverseKinematics(self.ROBOT_MAP[int(row[0])], 6, eef_pos, eef_orien)
+					self.p.resetBasePositionAndOrientation(self.ROBOT_MAP[int(row[0])] + 1, eef_pos, eef_orien)
 					for i in range(len(joint_pos)):
-						self.p.resetJointState(r, i, joint_pos[i])
+						self.p.resetJointState(self.ROBOT_MAP[int(row[0])], i, joint_pos[i])
 				if saveVideo:
 					self.video_capture()
 		self._exit_routine(f)
 
 
 	def video_capture(self):
-
-		# viewMatrix = self.p.computeViewMatrixFromYawPitchRoll((targetPosX, targetPosY, targetPosZ), dist, yaw, pitch, roll, 2)
-
-		# projectionMatrix = self.p.computeProjectionMatrixFOV(60, 600 / 540., .01, 1000.)
+		"""
+		This is very, very slow at the moment
+		"""
 		img_arr = self.p.getCameraImage(600, 540, self.viewMatrix, self.projectionMatrix)
 		np_img = np.reshape(img_arr[2], (img_arr[1], img_arr[0], 4)) / 255.
 
@@ -133,20 +153,27 @@ class BulletPhysicsVR(object):
 		plt.pause(0.001)
 
 	def set_camera_view(self, targetPosX, targetPosY, targetPosZ, roll, pitch, yaw, dist):
-		
+		"""
+		Set the view of camera; typically egocentric, or oblique
+		"""
 		self.FOCAL_POINT = (targetPosX, targetPosY,  targetPosZ)
 		self.PITCH = pitch
 		self.ROLL = roll
 		self.YAW = yaw
 		self.FOCAL_LENGTH = dist
-		self.viewMatrix = self.p.computeViewMatrixFromYawPitchRoll((targetPosX, targetPosY, targetPosZ), dist, yaw, pitch, roll, 2)
+		self.viewMatrix = self.p.computeViewMatrixFromYawPitchRoll((targetPosX, targetPosY, targetPosZ), 
+			dist, yaw, pitch, roll, self.UP_AX_IDX)
 		self.projectionMatrix = self.p.computeProjectionMatrixFOV(60, 600 / 540., .01, 1000.)
 
 
 	def _init_scene(self):
-
+		"""
+		Basic scene needed for running tasks
+		"""
 		self.p.resetSimulation()
 		self.p.setGravity(0,0,-9.81)
+		if self.hand:
+			self.VR_HAND_ID = self.p.loadMJCF("MPL/mpl2.xml")[0]
 		self.p.loadURDF("table/table.urdf", 1.000000,-0.200000,0.000000,0.000000,0.000000,0.707107,0.707107)
 		# self.p.loadURDF("jenga/jenga.urdf", 1.300000,0.200000,0.699990,-0.000005,0.707107,0.000006,0.707107)
 		# self.p.loadURDF("jenga/jenga.urdf", 1.200000,0.200000,0.699990,-0.000005,0.707107,0.000006,0.707107)
@@ -154,12 +181,14 @@ class BulletPhysicsVR(object):
 
 	def _setup_robot(self):
 		self.p.loadURDF("plane.urdf",0,0,0,0,0,0,1)
-		robot = self.p.loadURDF('kuka_iiwa/model_vr_limits.urdf', 1.4,-0.2,0.6,0,0,0,1)	
-		gripper = self.p.loadSDF('gripper/wsg50_one_motor_gripper_new_free_base.sdf')
-		return robot, gripper
+		pos = [0.28, -0.95]		# Original y-coord for the robot arms; need to match with VS src code
+		for i in range(len(self.GRIPPER_ID)):
+			self.ROBOT_MAP[self.GRIPPER_ID[i]] = self.p.loadURDF('kuka_iiwa/model_vr_limits.urdf', 1.4, pos[i], 0.6,0,0,0,1)
+			self.p.loadSDF('gripper/wsg50_one_motor_gripper_new_free_base.sdf')
 
 	def _init_task(self):
 		repo = {}
+		# Indicate the indices of the objects that need to be tracked in the first entry
 		repo[0] = [(3,7), ("cvgl/pole.urdf",0.80000,0.100000,0.699990,0.00000,0.0,0.00000,1), 
 			("cvgl/pole.urdf",0.80000,-0.200000,0.699990,0.000000,0.0,0.00000,1),
 			("cvgl/pole.urdf",0.80000,-0.500000,0.699990,0.00000,0.0,0.00000,1),
@@ -168,30 +197,18 @@ class BulletPhysicsVR(object):
 			("cvgl/torus_2.urdf",0.8,0.1,0.79999,1,0,0,1),
 			("cvgl/torus_3.urdf",0.8,0.1,0.84999,1,0,0,1),
 			("cvgl/torus_4.urdf",0.8,0.1,0.89999,0,0,0,1)]
+
+		repo[1] = [(0, 3), ("sphere_small.urdf",0.80000,-0.200000,0.699990,0.000000,0.0,0.00000,1),
+				("sphere_small.urdf",0.76000,-0.1400000,0.729990,0.000000,0.0,0.00000,1),
+				("sphere_small.urdf",0.83000,-0.520000,0.699990,0.000000,0.0,0.00000,1),
+				("tray/tray_textured2.urdf", 1.2, -0.2, 0.6, 0, 0, 0, 1)]
+
 		return repo
 
 	def _exit_routine(self, fp):
 		fp.close()
 		self.p.resetSimulation()
 		self.p.disconnect()
-
-# b = BulletPhysicsVR(p, 2)
-# b.record('see.csv')
-# b.replay('see.csv')
-
-
-# def set_camera_position():
-# 	# upAxisIndex: 1 for Y, 2 for Z
-# 	pass
-
-# def render_scene_full(intvl=10):
-# 	for pitch in range(0, 360, intvl):
-# 		viewMatrix = p.computeViewMatrixFromYawPitchRoll(FOCAL_POINT, FOCAL_LENGTH, YAW, pitch, ROLL, UP_AX_IDX)
-# 		projectionMatrix = p.computeProjectionMatrixFOV(60, 320 / 240., 0.01, 1000.)
-# 		img_arr = p.getCameraImage(320, 240, viewMatrix, projectionMatrix)
-# 		np_img = np.reshape(img_arr[2], (img_arr[1], img_arr[0], 4)) / 255.
-# 		plt.imshow(np_img)
-# 		plt.pause(0.01)
 
 # def render_camera_port(focus_pt, focal_len, yaw, pitch, roll, upAxisIndex=2):
 # 	viewMatrix = p.computeViewMatrixFromYawPitchRoll(focus_pt, focal_len, yaw, pitch, roll, upAxisIndex)
