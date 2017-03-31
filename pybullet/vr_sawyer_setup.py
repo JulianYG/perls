edf40880d3afe12ca117c086c65097ea1f64f2c9
Julian <yg1811@hotmail.com>
2017-03-31 15:34:56 +0800

    why?

--- pybullet/vr_sawyer_setup.py
+++ pybullet/vr_sawyer_setup.py
@@ -4,15 +4,23 @@ import math
 from datetime import datetime
 # p.connect(p.SHARED_MEMORY)
 
-LOWER_LIMITS = [0, 0, 0, 0, 0, -3.05, -5.1477, 0, 0, 0, -1.57079632679, -3.0514, 
-	-3.0514, -2.9842, 0, -2.9842, 0, 0, -2.9842, 0]
-UPPER_LIMITS = [0, 0, 0, 0, 0, 3.05, 0.9599, 0, 0, 0, 1.57079632679, 3.0514, 
-	3.0514, 2.9842, 0, 2.9842, 0, 0, 2.9842, 0]
-JOINT_RANGE = [0, 0, 0, 0, 0, 6, 6, 0, 0, 0, 3.14, 6, 6, 5.9, 0, 5.9, 0, 0, 5.9, 0]
+# LOWER_LIMITS = [0, 0, 0, 0, 0, -3.05, -5.1477, 0, 0, 0, -1.57079632679, -3.0514, 
+# 	-3.0514, -2.9842, 0, -2.9842, 0, 0, -2.9842, 0]
+# UPPER_LIMITS = [0, 0, 0, 0, 0, 3.05, 0.9599, 0, 0, 0, 1.57079632679, 3.0514, 
+# 	3.0514, 2.9842, 0, 2.9842, 0, 0, 2.9842, 0]
+# JOINT_RANGE = [0, 0, 0, 0, 0, 6, 6, 0, 0, 0, 3.14, 6, 6, 5.9, 0, 5.9, 0, 0, 5.9, 0]
 REST_POSE = [0, 0, 0, 0, 0, 0.00, 0, 0, 0, 0, -1.18, 0.00, 2.18, 0.00, 0, 0.57, 0, 0, 3.3161, 0]
-JOINT_DAMP = [0.1] * 20
+# JOINT_DAMP = [0.1] * 20
 
-MAX_FORCE = 500
+LOWER_LIMITS = [-3.0503, -5.1477, -3.8183, -3.0514, 
+	-3.0514, -2.9842, -2.9842, -4.7104]
+UPPER_LIMITS = [3.0503, 0.9599, 2.2824, 3.0514, 
+	3.0514, 2.9842, 2.9842, 4.7104]
+JOINT_RANGE = [6.1, 6.1, 6.1, 6.1, 6.1, 5.96, 5.96, 9.4]
+REST_POSE_IK = [0, 0, -1.18, 0.00, 2.18, 0.00, 0.57, 3.3161]
+JOINT_DAMP = [0.1] * 8
+
+MAX_FORCE = 1500
 
 p.connect(p.GUI)
 p.setRealTimeSimulation(1)
@@ -22,11 +30,14 @@ objects = [p.loadURDF("sawyer_robot/sawyer_description/urdf/sawyer.urdf", [0.0, 
 	0], useFixedBase=True)]
 sawyer = objects[0]
 p.resetBasePositionAndOrientation(sawyer, [0, 0, 0.912989899704], [0, 0, 0, 1])
-jointPositions=[0, 0, 0, 0, 0, 0.00, 0, 0, 0, 0, -1.18, 0.00, 2.18, 0.00, 0, 0.57, 0, 0, 3.3161, 0]
 
 for jointIndex in range (p.getNumJoints(sawyer)):
-	p.resetJointState(sawyer,jointIndex,jointPositions[jointIndex])
-	p.setJointMotorControl2(sawyer,jointIndex,p.POSITION_CONTROL,jointPositions[jointIndex],0)
+	qIndex = p.getJointInfo(sawyer, jointIndex)[3]
+	if qIndex > -1:
+		p.resetJointState(sawyer,jointIndex,REST_POSE[jointIndex])
+		p.setJointMotorControl2(sawyer, jointIndex, p.POSITION_CONTROL, 
+			targetPosition=REST_POSE[qIndex - 7], targetVelocity=0, 
+				positionGain=0.03, velocityGain=1.0, force=MAX_FORCE)
 
 objects = [p.loadURDF("lego/lego.urdf", 1.000000,-0.200000,0.700000,0.000000,0.000000,0.000000,1.000000)]
 objects = [p.loadURDF("lego/lego.urdf", 1.000000,-0.200000,0.800000,0.000000,0.000000,0.000000,1.000000)]
@@ -49,13 +60,13 @@ p.setGravity(0,0,-9.81)
 
 objects = [p.loadURDF("teddy_vhacd.urdf", 1.050000,-0.500000,0.700000,0.000000,0.000000,0.707107,0.707107)]
 objects = [p.loadURDF("cube_small.urdf", 0.950000,-0.100000,0.700000,0.000000,0.000000,0.707107,0.707107)]
-objects = [p.loadURDF("sphere_small.urdf", 0.850000,-0.400000,0.700000,0.000000,0.000000,0.707107,0.707107)]
+# objects = [p.loadURDF("sphere_small.urdf", 0.850000,-0.400000,0.700000,0.000000,0.000000,0.707107,0.707107)]
 objects = [p.loadURDF("duck_vhacd.urdf", 0.850000,-0.400000,0.900000,0.000000,0.000000,0.707107,0.707107)]
 # objects = p.loadSDF("kiva_shelf/model.sdf")
 # ob = objects[0]
 # p.resetBasePositionAndOrientation(ob,[0.000000,1.000000,1.204500],[0.000000,0.000000,0.000000,1.000000])
 objects = [p.loadURDF("teddy_vhacd.urdf", -0.100000,0.600000,0.850000,0.000000,0.000000,0.000000,1.000000)]
-objects = [p.loadURDF("sphere_small.urdf", -0.100000,0.955006,1.169706,0.633232,-0.000000,-0.000000,0.773962)]
+ball = p.loadURDF("sphere_small.urdf", -0.100000,0.955006,1.169706,0.633232,-0.000000,-0.000000,0.773962)
 objects = [p.loadURDF("cube_small.urdf", 0.300000,0.600000,0.850000,0.000000,0.000000,0.000000,1.000000)]
 objects = [p.loadURDF("table_square/table_square.urdf", -1.000000,0.000000,0.000000,0.000000,0.000000,0.000000,1.000000)]
 ob = objects[0]
@@ -68,22 +79,6 @@ ob = objects[0]
 jointPositions=[ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 ]
 for jointIndex in range (p.getNumJoints(ob)):
 	p.resetJointState(ob,jointIndex,jointPositions[jointIndex])
-
-# LOWER_LIMITS = [-3.05, -5.1477, -1.57079632679, -3.0514, 
-# 	-3.0514, -2.9842, -2.9842, -2.9842]
-# UPPER_LIMITS = [3.05, 0.9599, 1.57079632679, 3.0514, 
-# 	3.0514, 2.9842, 2.9842, 2.9842]
-# JOINT_RANGE = [6.1, 6.1, math.pi, 6.1, 6.1, 5.96, 5.96, 5.96]
-# REST_POSE = [0, 0, -1.18, 0.00, 2.18, 0.00, 0.57, 3.3161]
-# JOINT_DAMP = [0.1] * 8
-
-# for jointIndex in range(p.getNumJoints(sawyer)):
-# 	jointInfo = p.getJointInfo(sawyer, jointIndex)
-# 	# If flag
-# 	if jointInfo[5]:
-# 		p.setJointMotorControl2(sawyer, jointInfo[3] - 7, p.POSITION_CONTROL, 
-# 			targetPosition=REST_POSE[jointIndex], targetVelocity=0, positionGain=0.03,
-# 			velocityGain=1.0, force=MAX_FORCE)
 
 (0, 'controller_box_fixed', 4, -1, -1, 0, 0.0, 0.0)
 (1, 'pedestal_feet_fixed', 4, -1, -1, 0, 0.0, 0.0)
@@ -106,95 +101,169 @@ for jointIndex in range (p.getNumJoints(ob)):
 (18, 'right_j6', 0, 14, 13, 1, 0.0, 0.0)
 (19, 'right_hand', 4, -1, -1, 0, 0.0, 0.0)
 
+# a = p.createConstraint(sawyer, -1, sawyer, 0, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
+# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
+# 	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
+# b = p.createConstraint(sawyer, -1, sawyer, 1, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
+# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
+# 	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
+# c = p.createConstraint(sawyer, -1, sawyer, 2, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
+# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
+# 	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
+# d = p.createConstraint(sawyer, -1, sawyer, 3, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
+# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
+# 	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
+# e = p.createConstraint(sawyer, -1, sawyer, 4, p.JOINT_FIXED, [0.000000,0.000000,0.000000],
+# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
+# 	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
 
-p.createConstraint(sawyer, -1, sawyer, 0, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
-	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
-	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
-p.createConstraint(sawyer, -1, sawyer, 1, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
-	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
-	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
-p.createConstraint(sawyer, -1, sawyer, 2, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
-	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
-	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
-p.createConstraint(sawyer, -1, sawyer, 3, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
-	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
-	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
-p.createConstraint(sawyer, -1, sawyer, 4, p.JOINT_FIXED, [0.000000,0.000000,0.000000],
-	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
-	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
-p.createConstraint(sawyer, 5, sawyer, 6, p.JOINT_FIXED, [0.000000,0.000000,0.000000],
-	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
-	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
-# p.createConstraint(sawyer, 6, sawyer, 7, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
-# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
-# 	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
-# p.createConstraint(sawyer, 6, sawyer, 8, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
-# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
-# 	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
-p.createConstraint(sawyer, 5, sawyer, 9, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
-	[0.000000,0.00000,0.00000],[0.000000,0.0000,0.00000],
-	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
-
-# p.createConstraint(sawyer, 13, sawyer, 14, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
-# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
-# 	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
-# p.createConstraint(sawyer, 15, sawyer, 16, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
-# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
-# 	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
-# p.createConstraint(sawyer, 15, sawyer, 17, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
-# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000],
-# 	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
-# p.createConstraint(sawyer, 18, sawyer, 19, p.JOINT_FIXED,[0.000000,0.000000,0.000000],
-# 	[0.000000,0.00000,0.000000],[0.000000,0.00000,0.000000],
-# 	[0.000000,0.000000,0.000000,1.000000],[0.000000,0.000000,0.000000,1.000000])
+# p.createConstraint(sawyer, 5, sawyer, 9, p.JOINT_FIXED, [0.000000,0.000000,0.000000],
+# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000])
+# p.createConstraint(sawyer, 6, sawyer, 7, p.JOINT_FIXED, [0.000000,0.000000,0.000000],
+# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000])
+# p.createConstraint(sawyer, 6, sawyer, 8, p.JOINT_FIXED, [0.000000,0.000000,0.000000],
+# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000])
+# p.createConstraint(sawyer, 13, sawyer, 14, p.JOINT_FIXED, [0.000000,0.000000,0.000000],
+# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000])
+# p.createConstraint(sawyer, 15, sawyer, 16, p.JOINT_FIXED, [0.000000,0.000000,0.000000],
+# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000])
+# p.createConstraint(sawyer, 15, sawyer, 17, p.JOINT_FIXED, [0.000000,0.000000,0.000000],
+# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000])
+# p.createConstraint(sawyer, 18, sawyer, 19, p.JOINT_FIXED, [0.000000,0.000000,0.000000],
+# 	[0.000000,0.00000,0.00000],[0.000000,0.00000,0.00000])
 
 prevPose=[0,0,0]
 prevPose1=[0,0,0]
 hasPrevPose = 0
-# print(p.getLinkState(sawyer, 19)[1])
-# p.setJointMotorControl2(sawyer, 10, p.POSITION_CONTROL, 
-# 	targetPosition= -math.pi / 2, targetVelocity=0, positionGain=0.03, velocityGain=1.0,
-# 	force=MAX_FORCE)
-pos = [0.67, 0.2, 1.4]
-p.addUserDebugLine(pos,[0.6, 0.2, 1.5],[1,0,0],100,0)
-orn = (0.707, 0.707, 0, 0)
-joint_pos = p.calculateInverseKinematics(sawyer, 19, pos, orn,
-		lowerLimits=LOWER_LIMITS, upperLimits=UPPER_LIMITS, 
-		jointRanges=JOINT_RANGE, restPoses=REST_POSE, jointDamping=JOINT_DAMP)
 
-for jointIndex in range(p.getNumJoints(sawyer)):
-	qIndex = p.getJointInfo(sawyer, jointIndex)[3]
-	if qIndex > -1:
-		p.setJointMotorControl2(sawyer, jointIndex, p.POSITION_CONTROL, 
-			targetPosition=joint_pos[qIndex - 7], targetVelocity=0, 
-			positionGain=0.03, velocityGain=1.0, force=MAX_FORCE)
-print(joint_pos)
-print (p.getLinkState(sawyer, 19)[4], pos)
+pos = [2.4, 0.3, 1.2]
+# orn = [0.707, 0.707, 0, 0]
+
+F2_pos = -0.59
+F3_pos = 0.0
+F4_pos = -0.052047
+F5_pos = 0.33763
+F6_pos = 0.11683
+F7_pos = 0.40877
+F8_pos = 0.2857
+F9_pos = 0.35124
+
+
+# print(joint_pos)
+
+# import ikpy
+# urdf_file = "../../data/sawyer_robot/sawyer_description/urdf/sawyer.urdf"
+# # urdf_file = "../../data/kuka_iiwa/model_vr_limits.urdf"
+# my_sawyer = ikpy.chain.Chain.from_urdf_file(urdf_file)
+
+# target_vector = [ 0.6, 0.1, 0.8]
+# target_frame = np.eye(4)
+# target_frame[:3, 3] = target_vector
+# print(my_sawyer.inverse_kinematics(target_frame))
+def euc_dist(posA, posB):
+	dist = 0.
+	for i in range(len(posA)):
+		dist += (posA[i] - posB[i]) ** 2
+	return dist
+
+
 while True:
 
 	dt = datetime.now()
 	t = (dt.second/60.)*2.*math.pi
 	# pos = [0.5, 0.2*math.cos(t),1.2+0.2*math.sin(t)]
-	pos = [0.6, 0.2, 1.4]
-	p.addUserDebugLine(pos,[0.6, 0.2, 1.5],[1,0,0],100,0)
-	orn = (0.707, 0.707, 0, 0)
+	# pos = [0.6, 0.2, 1.4]
+	# p.addUserDebugLine(pos,[0.6, 0.2, 1.5],[1,0,0],100,0)
+	# orn = (0.707, 0.707, 0, 0)
 
-	# joint_pos = p.calculateInverseKinematics(sawyer, 19, pos, orn,
+	# joint_pos = p.calculateInverseKinematics(sawyer, 17, pos, orn,
 	# 	lowerLimits=LOWER_LIMITS, upperLimits=UPPER_LIMITS, 
-	# 	jointRanges=JOINT_RANGE, restPoses=REST_POSE, jointDamping=JOINT_DAMP)
+	# 	jointRanges=JOINT_RANGE, restPoses=REST_POSE_IK, jointDamping=JOINT_DAMP)
 
-	# for jointIndex in range(p.getNumJoints(sawyer)):
-	# 	qIndex = p.getJointInfo(sawyer, jointIndex)[3]
-	# 	if qIndex > -1:
-	# 		p.setJointMotorControl2(sawyer, jointIndex, p.POSITION_CONTROL, 
-	# 			targetPosition=joint_pos[qIndex - 7], targetVelocity=0, 
-	# 			positionGain=0.03, velocityGain=1.0, force=MAX_FORCE)
 
-	ls = p.getLinkState(sawyer, 19)
-	if hasPrevPose:
-		p.addUserDebugLine(prevPose, pos, [0,0,0.3], 1, 60)
-		p.addUserDebugLine(prevPose1, ls[4], [1,0,0], 1, 60)
-	prevPose=pos
-	prevPose1=ls[4]
-	hasPrevPose=1
+
+	
+	pos = p.getBasePositionAndOrientation(ball)[0]
+	sq_len = euc_dist(p.getLinkState(sawyer, 18)[0], pos)		
+	if sq_len < 1.3 * 1.3:
+		joint_pos = p.calculateInverseKinematics(sawyer, 18, pos,
+		lowerLimits=LOWER_LIMITS, upperLimits=UPPER_LIMITS, 
+		jointRanges=JOINT_RANGE, restPoses=REST_POSE_IK, jointDamping=JOINT_DAMP)
+
+
+		for jointIndex in range(p.getNumJoints(sawyer)):
+			qIndex = p.getJointInfo(sawyer, jointIndex)[3]
+			if qIndex > -1:
+				p.setJointMotorControl2(sawyer, jointIndex, p.POSITION_CONTROL, 
+					targetPosition=joint_pos[qIndex - 7], targetVelocity=0, 
+					positionGain=0.03, velocityGain=1.0, force=MAX_FORCE)
+
+	else:
+		for jointIndex in range(p.getNumJoints(sawyer)):
+			qIndex = p.getJointInfo(sawyer, jointIndex)[3]
+		if qIndex > -1:
+			p.setJointMotorControl2(sawyer, jointIndex, p.POSITION_CONTROL, 
+				targetPosition=REST_POSE[qIndex - 7], targetVelocity=0, 
+				positionGain=0.03, velocityGain=1.0, force=MAX_FORCE)
+
+	# jj = [p.getJointState(sawyer, i)[0] for i in range(20)]
+	# print([jj[5], jj[6], jj[10], jj[11], jj[12], jj[13], jj[15], jj[18]])
+	# print(joint_pos)
+	# print(pos)
+	# print (p.getLinkState(sawyer, 18)[4])
+
+	# ls = p.getLinkState(sawyer, 19)
+	# # print(ls)
+	# if hasPrevPose:
+	# 	p.addUserDebugLine(prevPose, pos, [0,0,0.3], 1, 60)
+	# 	p.addUserDebugLine(prevPose1, ls[4], [1,0,0], 1, 60)
+	# prevPose=pos
+	# prevPose1=ls[4]
+	# hasPrevPose=1
+	
+
+	# events = p.getKeyboardEvents()
+ # 	for e in events:
+	# 	if e == p.B3G_F2 and (events[e] == p.KEY_IS_DOWN):
+	# 		p.setJointMotorControl2(sawyer, 5, p.POSITION_CONTROL, 
+	# 			targetPosition=F2_pos, targetVelocity=0, positionGain=0.03,
+	# 				velocityGain=1.0, force=MAX_FORCE)
+	# 		# F2_pos -= 0.1
+
+	# 	if e == p.B3G_F3 and (events[e] == p.KEY_IS_DOWN):
+	# 		p.setJointMotorControl2(sawyer, 6, p.POSITION_CONTROL, 
+	# 			targetPosition=F3_pos, targetVelocity=0, positionGain=0.03,
+	# 				velocityGain=1.0, force=MAX_FORCE)
+	# 		# F3_pos -= 0.1
+	# 	if e == p.B3G_F4 and (events[e] == p.KEY_IS_DOWN):
+	# 		p.setJointMotorControl2(sawyer, 10, p.POSITION_CONTROL, 
+	# 			targetPosition=F4_pos, targetVelocity=0, positionGain=0.03,
+	# 				velocityGain=1.0, force=MAX_FORCE)
+	# 		# F4_pos -= 0.1
+	# 	if e == p.B3G_F5 and (events[e] == p.KEY_IS_DOWN):
+	# 		p.setJointMotorControl2(sawyer, 11, p.POSITION_CONTROL, 
+	# 			targetPosition=F5_pos, targetVelocity=0, positionGain=0.03,
+	# 				velocityGain=1.0, force=MAX_FORCE)
+	# 		# F5_pos -= 0.1
+	# 	if e == p.B3G_F6 and (events[e] == p.KEY_IS_DOWN):
+	# 		p.setJointMotorControl2(sawyer, 12, p.POSITION_CONTROL, 
+	# 			targetPosition=F6_pos, targetVelocity=0, positionGain=0.03,
+	# 				velocityGain=1.0, force=MAX_FORCE)
+	# 		# F6_pos -= 0.1
+	# 	if e == p.B3G_F7 and (events[e] == p.KEY_IS_DOWN):
+	# 		p.setJointMotorControl2(sawyer, 13, p.POSITION_CONTROL, 
+	# 			targetPosition=F7_pos, targetVelocity=0, positionGain=0.03,
+	# 				velocityGain=1.0, force=MAX_FORCE)
+	# 		# F7_pos -= 0.1
+	# 	if e == p.B3G_F8 and (events[e] == p.KEY_IS_DOWN):
+	# 		p.setJointMotorControl2(sawyer, 15, p.POSITION_CONTROL, 
+	# 			targetPosition=F8_pos, targetVelocity=0, positionGain=0.03,
+	# 				velocityGain=1.0, force=MAX_FORCE)
+	# 		# F8_pos -= 0.1
+	# 	if e == p.B3G_F9 and (events[e] == p.KEY_IS_DOWN):
+	# 		p.setJointMotorControl2(sawyer, 18, p.POSITION_CONTROL, 
+	# 			targetPosition=F9_pos, targetVelocity=0, positionGain=0.03,
+	# 				velocityGain=1.0, force=MAX_FORCE)
+	# 		# F9_pos -= 0.1
+
 	p.stepSimulation()
