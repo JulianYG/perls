3ac10cda085ba60eaa50faa480e5f91a6d4810b4
jygao <yg1811@hotmail.com>
2017-06-01 17:32:41 -0700

    recalib

--- tools/calibration/uvc_camera.py
+++ tools/calibration/uvc_camera.py
@@ -12,7 +12,17 @@ cam = VideoCapture(0)   # 0 -> index of camera
 cam.set(3, 1280)
 cam.set(4, 720)
 
-tracker = Tracker(None, None, (9,6))
+_UK = np.array([
+	[927.902447 ,0.000000 ,641.850659],
+	[0.000000, 921.598756, 345.336021],
+	[0.000000, 0.000000 ,1.000000]
+	], dtype=np.float32)
+
+_UD = np.array([
+	0.078759, -0.143339, -0.000887 ,-0.001555 ,0.000000
+	], dtype=np.float32)
+
+tracker = Tracker(None, None, K=_UK, D=_UD, board_size=(9,6))
 
 def mouse_callback(event, x, y, flags, params):
 	if event == 1:
