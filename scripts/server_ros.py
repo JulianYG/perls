5cde302c6bd6cf78c19ee0e03c1904ecd330ee6f
Julian <yg1811@hotmail.com>
2017-06-17 15:11:01 -0700

    orn flag ;

--- scripts/server_ros.py
+++ scripts/server_ros.py
@@ -26,6 +26,8 @@ LOWER_LIMITS = [-3.05, -3.82, -3.05, -3.05, -2.98, -2.98, -4.71]
 UPPER_LIMITS = [3.05, 2.28, 3.05, 3.05, 2.98, 2.98, 4.71]
 JOINT_RANGE = [6.1, 6.1, 6.1, 6.1, 5.96, 5.96, 9.4]	
 
+FIXED = True
+
 p.connect(p.GUI)
 sawyer = p.loadURDF('sawyer_robot/sawyer_description/urdf/sawyer_arm.urdf',
 	(0, 0, 0.9), useFixedBase=1)
@@ -49,7 +51,6 @@ class VR(object):
 	def __init__(self):
 		self.vr_initial_pos = None
 
-
 		limb = intera_interface.Limb('right')
 		gripper = intera_interface.Gripper('right')
 
@@ -59,7 +60,6 @@ class VR(object):
 
 		self.r = redis.StrictRedis(host='localhost', port=6379, db=0)
 		self.pubsub = self.r.pubsub()
-		# sampler = UniformSubsampler(T=50)
 
 	def turn_on(self):
 
@@ -99,35 +99,31 @@ class VR(object):
 			self.vr_initial_pos = np.array(list(e[1]))
 			self.controller.put_item(rest_pose.values()[::-1])
 
-		# self.arm_target = self.arm_init + (vr_now - vr_init)
 		rel_pos = np.array(pos) - np.array(self.vr_initial_pos)
-
 
 		if np.sum(rel_pos ** 2) >= 2.0:
 			print(rel_pos, 'wrong o')
 			return
-		# sim_rel_pos = np.array([rel_pos[1], rel_pos[0], rel_pos[2]])
 
 		self.arm_target_pos = self.arm_initial_pos + rel_pos
-
-
 
 		# Use pybullet for now...
 		sim_target_pos = sim_initial_pos + rel_pos
 
 		t = time.time() - self.prev_time
 
-		self.arm_joint_pos = p.calculateInverseKinematics(sawyer, 6, 
+		self.arm_joint_pos = list(p.calculateInverseKinematics(sawyer, 6, 
 						sim_target_pos, (0, 1, 0, 0), lowerLimits=LOWER_LIMITS, 
 						upperLimits=UPPER_LIMITS, jointRanges=JOINT_RANGE, 
-						restPoses=REST_POSE, jointDamping=[0.1] * 7)
+						restPoses=REST_POSE, jointDamping=[0.1] * 7))
 
 		jpos = self.arm.get_joint_angles().values()[::-1]
 
-		x, y, _ = p.getEulerFromQuaternion(orn)
+		if not FIXED:
+			x, y, _ = p.getEulerFromQuaternion(orn)
 
-		self.arm_joint_pos[5] = np.clip(x, LOWER_LIMITS[5], UPPER_LIMITS[5])
-		self.arm_joint_pos[6] = np.clip(y, LOWER_LIMITS[6], UPPER_LIMITS[6])
+			self.arm_joint_pos[5] = np.clip(x, LOWER_LIMITS[5], UPPER_LIMITS[5])
+			self.arm_joint_pos[6] = np.clip(y, LOWER_LIMITS[6], UPPER_LIMITS[6])
 
 		for i in range(7):
 
@@ -139,7 +135,6 @@ class VR(object):
 				force = 500, 
 				positionGain=0.05,
 				velocityGain = 1.)
-		# if 0.1 < np.sqrt(np.sum(self.arm_target_pos ** 2)) < 0.8:
 
 		self.controller.put_item((self.arm_joint_pos, t))
 		self.prev_time = time.time()
