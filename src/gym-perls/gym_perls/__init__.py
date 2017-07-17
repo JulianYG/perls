677144d5a50241a0547ea2426c8c6b42540259fd
Julian <yg1811@hotmail.com>
2017-07-17 01:26:10 -0700

    optimize gym env

--- src/gym-perls/gym_perls/__init__.py
+++ src/gym-perls/gym_perls/__init__.py
@@ -1,14 +1,15 @@
 from gym.envs.registration import register
 import os.path as osp
 
+
 register(
     id='perls-v0',
-    entry_point='envs:PerlsEnv',
+    entry_point='gym_perls.envs:PerlsEnv',
     kwargs=dict(conf_path=osp.abspath(osp.join(__file__, '../gym.xml'))),
 )
 
 register(
     id='exp-v0',
-    entry_point='envs:PushCube',
+    entry_point='gym_perls.envs:PushCube',
     kwargs=dict(conf_path=osp.abspath(osp.join(__file__, '../gym.xml'))),
 )
--- src/gym-perls/gym_perls/envs/__init__.py
+++ src/gym-perls/gym_perls/envs/__init__.py
@@ -1,3 +1,4 @@
 # ------------perls------------
+
 from .perls_env import PerlsEnv
 from .push_cube import PushCube
