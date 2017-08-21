a5d324fecac1d769cf344befa4362977c18893af
Julian <yg1811@hotmail.com>
2017-08-21 15:15:52 -0700

    simplify gym

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 
-from src.lib import entity
-from src.lib.control import Controller
-from src.lib.view import View
+from src.gym import gym_perls as gym
+
+# from src.lib import entity
+# from src.lib.control import Controller
+# from src.lib.view import View
 # from src.lib.world import World
 # import src.lib.entity
 #
