9cc120dde379b52f38a52337bb7a04c151a4613c
Julian <yg1811@hotmail.com>
2017-09-14 12:50:35 -0700

    set goal bound; fix postprocessing

--- src/gym_/gym_perls/envs/push_pose.py
+++ src/gym_/gym_perls/envs/push_pose.py
@@ -25,20 +25,27 @@ class PushCubePose(PushCube):
             (table_abs_upper_bound, table_orn), self._robot.pose)
         table_lower = math_util.get_relative_pose(
             (table_abs_lower_bound, table_orn), self._robot.pose)
-        goal_pos = self._world.get_task_state()['goal']
+        
+        goal_abs_lower = math_util.vec((cube.pos[0] + 0.25, cube.pos[1] - 0.25, 0.641))
+        goal_abs_upper = math_util.vec((cube.pos[0] + 0.45, cube.pos[1] + 0.25, 0.642))
+
+        goal_upper = math_util.get_relative_pose(
+            (goal_abs_upper, table_orn), self._robot.pose)
+        goal_lower = math_util.get_relative_pose(
+            (goal_abs_lower, table_orn), self._robot.pose)
 
         return PushCube.Space.Box(
             low=math_util.concat((
                 self._robot.pos - math_util.vec((1.5, 1.5, 1.5)),
                 table_lower,
                 math_util.vec((-1, -1, -1, -1)),
-                goal_pos)
+                goal_lower)
             ),
             high=math_util.concat((
                 self._robot.pos + math_util.vec((1.5, 1.5, 1.5)),
                 table_upper,
                 math_util.vec((1, 1, 1, 1)),
-                goal_pos)
+                goal_upper)
             )
         )
 
--- src/gym_/gym_perls/envs/push_torq.py
+++ src/gym_/gym_perls/envs/push_torq.py
@@ -25,7 +25,13 @@ class PushCubeTorque(PushCube):
         table_lower = math_util.get_relative_pose(
             (table_abs_lower_bound, table_orn), self._robot.pose)
 
-        goal_pos = self._world.get_task_state()['goal']
+        goal_abs_lower = math_util.vec((cube.pos[0] + 0.25, cube.pos[1] - 0.25, 0.641))
+        goal_abs_upper = math_util.vec((cube.pos[0] + 0.45, cube.pos[1] + 0.25, 0.642))
+
+        goal_upper = math_util.get_relative_pose(
+            (goal_abs_upper, table_orn), self._robot.pose)
+        goal_lower = math_util.get_relative_pose(
+            (goal_abs_lower, table_orn), self._robot.pose)
 
         return PushCube.Space.Box(
             low=math_util.concat((
@@ -33,14 +39,14 @@ class PushCubeTorque(PushCube):
                 -math_util.vec(self._robot.joint_specs['max_vel']),
                 table_lower,
                 (-1, -1, -1, -1),
-                goal_pos)
+                goal_lower)
             ),
             high=math_util.concat((
                 math_util.vec(self._robot.joint_specs['upper']),
                 math_util.vec(self._robot.joint_specs['max_vel']),
                 table_upper,
                 (1, 1, 1, 1),
-                goal_pos)
+                goal_upper)
             )
         )
 
